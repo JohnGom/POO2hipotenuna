aad47abe8f860516d2de0a70a3382a2a8e41f0f0
USUARIO <USUARIO@192.168.51.212>
2014-03-16 10:30:05 -0500

    repositorio prueba Hipotenusa

---
+++ src/pruebahiportenusa/Hipotenusa.java
@@ -0,0 +1,26 @@
+package pruebahiportenusa;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Hipotenusa {
+    
+    
+    
+     public  double hipotenusa (double lado1, double lado2){
+         
+         double hipotenusa;
+         
+         hipotenusa= Math.sqrt(Math.pow(lado2,2)+ Math.pow(lado1, 2));
+         
+         return hipotenusa;
+     }
+    
+    
+}
---
+++ src/pruebahiportenusa/PruebaHiportenusa.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebahiportenusa;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class PruebaHiportenusa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Tabla n = new Tabla();
+        n.setVisible(true);
+    }
+}
